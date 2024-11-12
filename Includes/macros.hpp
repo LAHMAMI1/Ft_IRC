d3db249cf04c31123b9b397e49732c8fc4e4e79f
Oussama Lahmami <o.lahmami.2000@gmail.com>
2024-11-12 22:06:42 +0100

    creating KICK command

--- Includes/macros.hpp
+++ Includes/macros.hpp
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 09:59:58 by olahmami          #+#    #+#             */
-/*   Updated: 2024/11/10 12:15:34 by olahmami         ###   ########.fr       */
+/*   Updated: 2024/11/12 21:48:21 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,5 @@
 #define RPL_NOTOPIC(channelName) (":irc.chat.com 331 " + (channelName) + " :No topic is set\r\n")
 #define RPL_ENDOFNAMES(channelName, nickName) (":irc.chat.com 366 " + (nickName) + " " + (channelName) + " :End of /NAMES list\r\n")
 #define RPL_TOPIC(channelName, topic) (":irc.chat.com 332 " + (channelName) + " :" + (topic) + "\r\n")
-// #define RPL_TOPICWHOTIME(channelName, nickName, topic) (":irc.chat.com 333 " + (channelName) + " " + (nickName) + " " + getCurrentTime() + " :" + (topic) + "\r\n")
-// #define ERR_NOSUCHCHANNEL(channelName) (":irc.chat.com 403 " + (channelName) + " :No such channel\r\n")
+#define ERR_NOSUCHCHANNEL(channelName) (":irc.chat.com 403 " + (channelName) + " :No such channel\r\n")
+#define ERR_USERNOTINCHANNEL(nickName, channelName) (":irc.chat.com 441 " + (nickName) + " " + (channelName) + " :They aren't on that channel\r\n")
--- Server.cpp
+++ Server.cpp
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 11:59:09 by olahmami          #+#    #+#             */
-/*   Updated: 2024/11/10 12:14:15 by olahmami         ###   ########.fr       */
+/*   Updated: 2024/11/12 22:04:44 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -257,6 +257,56 @@ void Server::server(int ac, char **av)
                             it->second.setTopic(newTopic.substr(1));
                             std::string setTopic = RPL_TOPIC(channelName, newTopic.substr(1));
                             send(clients[clientIndex].getClientSocket(), setTopic.c_str(), setTopic.size(), 0);
+                        }
+                    }
+                    
+                    else if (command == "KICK")
+                    {
+                        std::string channelName;
+                        iss >> channelName;
+                        std::string nickName;
+                        iss >> nickName;
+                        std::string comment;
+                        std::getline(iss, comment);
+                        trim(comment);
+                        std::map<std::string, Channel>::iterator it = channels.find(channelName);
+                        
+                        if (NEEDMOREPARAMS(message, clients[clientIndex].getClientSocket(), 3))
+                            return;
+                        else if (channelName[0] != '#' || channelName.find(",") != std::string::npos)
+                        {
+                            std::cout << "Client " << clients[clientIndex].getClientSocket() << " :Invalid channel name" << std::endl;
+                            std::string errorMsg = "Invalid channel name\n";
+                            send(clients[clientIndex].getClientSocket(), errorMsg.c_str(), errorMsg.size(), 0);
+                        }
+                        else if (it == channels.end())
+                        {
+                            std::string errorMsg = ERR_NOSUCHCHANNEL(channelName);
+                            send(clients[clientIndex].getClientSocket(), errorMsg.c_str(), errorMsg.size(), 0);
+                        }
+                        else if (comment.empty())
+                            comment = "FOR NO REASON";
+                        else
+                        {
+                            std::map<int, std::string>& users = it->second.getUsers();
+                            bool userFound = false;
+
+                            for (std::map<int, std::string>::iterator it = users.begin(); it != users.end(); ++it)
+                            {
+                                if (it->second == nickName)
+                                {
+                                    std::string kickMessage = clients[clientIndex].getNickName() + "KICK " + channelName + " " + nickName + " :" + comment + "\r\n";
+                                    send(it->first, kickMessage.c_str(), kickMessage.size(), 0);
+                                    users.erase(it);
+                                    userFound = true;
+                                    break;
+                                }
+                            }
+                            if (!userFound)
+                            {
+                                std::string errorMsg = ERR_USERNOTINCHANNEL(nickName, channelName);
+                                send(clients[clientIndex].getClientSocket(), errorMsg.c_str(), errorMsg.size(), 0);
+                            }
                         }
                     }
                 }
